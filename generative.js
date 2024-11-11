97cc36ea1277080f71d0f887b35b5301bc989c6e
Kundaniel Ouk <kundaniel.ouk@student.ehb.be>
2024-11-11 14:24:05 +0100

    Created line of triangles without the wrong code

--- generative.js
+++ generative.js
@@ -27,23 +27,6 @@ function drawTriangle(x, y, size) {
   context.stroke();
 }
 
-function drawMultipleLinesOfTriangles() {
-  let canvasWidth = canvas.width;
-  let canvasHeight = canvas.height;
-
-  let margin = 0.05 * canvasWidth;
-  let size = 0.1 * canvasWidth;
-  let spacing = 0.15 * canvasWidth;
-  let numberOfTriangles = Math.floor((canvasWidth - 2 * margin) / spacing);
-
-  let startY = canvasHeight * 0.5;
-
-  for (let i = 0; i < numberOfTriangles; i++) {
-    let x = margin + i * spacing;
-    drawTriangle(x, startY, size);
-  }
-}
-
 function drawLineOfTriangles() {
   let canvasWidth = canvas.width;
   let canvasHeight = canvas.height;
