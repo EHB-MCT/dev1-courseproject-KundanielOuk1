33fa90a7395f077d37e1314abbe8766cebc6b846
Kundaniel Ouk <kundaniel.ouk@student.ehb.be>
2024-12-18 13:40:44 +0100

    Oude signature in bestand plaatsen

--- iterative.js
+++ iterative.js
@@ -1,3 +1,26 @@
 "use strict";
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
+
+signature();
+
+function signature() {
+  context.beginPath();
+  context.fillStyle = "#f4f4f4";
+  context.fillRect(0, 0, 500, 500);
+
+  context.fillStyle = "#e4848c";
+  context.fillRect(40, 40, 85, 85);
+
+  context.fillRect(210, 40, 85, 85);
+
+  context.fillRect(380, 40, 85, 85);
+
+  context.fillRect(40, 380, 85, 85);
+
+  context.fillRect(210, 380, 85, 85);
+
+  context.fillRect(380, 380, 85, 85);
+
+  context.fillRect(40, 295, 425, 85);
+}
