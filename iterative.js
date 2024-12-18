4a05012a723bec9f7adeebfedd7d8ba04f1fb956
Kundaniel Ouk <kundaniel.ouk@student.ehb.be>
2024-12-18 15:49:44 +0100

    Code voor het aantal driehoeken

--- iterative.js
+++ iterative.js
@@ -5,6 +5,36 @@ import * as Utils from "../../scripts/utils.js";
 let canvasWidth = context.canvas.width;
 let canvasHeight = context.canvas.height;
 
+let triangles = [];
+
+let mouseX = width / 2;
+let mouseY = height / 2;
+
+canvas.addEventListener("mousemove", (event) => {
+  let rect = canvas.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+});
+
+createNumTriangles();
+triangles();
+animate();
+
+//100 driehoeken maken
+function createNumTriangles() {
+  for (let i = 0; i < 100; i++) {
+    let size = Math.random() * 30 + 10; // Willekeurige grootte tussen 10 en 40
+    triangles.push({
+      x: Math.random() * width,
+      y: Math.random() * height,
+      size: size,
+      speed: (Math.random() * 1 + 0.5) * 0.05, // Snelheid
+      upward: Math.random() > 0.5, // willekeurig omhoog of omlaag driehoek
+      color: generateRandomColor(Math.random() > 0.5 ? 0 : 250), // Rood of blauwe hue
+    });
+  }
+}
+
 signature();
 
 function signature() {
