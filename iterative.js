386299916ae416078a75cc80cbfd5a78e51d4a06
Kundaniel Ouk <kundaniel.ouk@student.ehb.be>
2024-12-18 14:03:34 +0100

    Signature rechts onderaan geplaatst

--- iterative.js
+++ iterative.js
@@ -2,25 +2,25 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
+let canvasWidth = context.canvas.width;
+let canvasHeight = context.canvas.height;
+
 signature();
 
 function signature() {
+  let right = canvasWidth - 500;
+  let bottom = canvasHeight - 500;
+
   context.beginPath();
   context.fillStyle = "#f4f4f4";
-  context.fillRect(0, 0, 500, 500);
+  context.fillRect(right, bottom, 500, 500);
 
   context.fillStyle = "#e4848c";
-  context.fillRect(40, 40, 85, 85);
-
-  context.fillRect(210, 40, 85, 85);
-
-  context.fillRect(380, 40, 85, 85);
-
-  context.fillRect(40, 380, 85, 85);
-
-  context.fillRect(210, 380, 85, 85);
-
-  context.fillRect(380, 380, 85, 85);
-
-  context.fillRect(40, 295, 425, 85);
+  context.fillRect(right + 40, bottom + 40, 85, 85);
+  context.fillRect(right + 210, bottom + 40, 85, 85);
+  context.fillRect(right + 380, bottom + 40, 85, 85);
+  context.fillRect(right + 40, bottom + 380, 85, 85);
+  context.fillRect(right + 210, bottom + 380, 85, 85);
+  context.fillRect(right + 380, bottom + 380, 85, 85);
+  context.fillRect(right + 40, bottom + 295, 425, 85);
 }
